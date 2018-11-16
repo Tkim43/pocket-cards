7bce3ea3500c4bb940f51594c09a55cdc0c073ca
viennaltran <lanvienhtran@gmail.com>
2018-11-15 20:37:29 -0800

    completed part 3 of MERN set up

--- project_setup/mern_setup/client/src/components/test.js
+++ project_setup/mern_setup/client/src/components/test.js
@@ -3,16 +3,28 @@ import axios from 'axios';
 
 class Test extends Component {
     async componentDidMount () {
-        const resp = await axios.get('/api/test');
+        const sendToServer={
+            email:'jim@mail.com',
+            password:'asdf1234'
 
-        console.log('Server response: ', resp);
+        };
 
-        const user = await axios.get('/api/user');
+        const signInResp = await axios.post('/api/sign-in',sendToServer);
 
-        console.log('user response: ', user);
+        console.log('Sign In Response:', signInResp);
+
+
+
+        // const resp = await axios.get('/api/test');
+
+        // console.log('Server response: ', resp);
+
+        // const user = await axios.get('/api/user');
+
+        // console.log('user response: ', user);
     }
     render () {
-        return <h1>This is the Test component</h1>;
+        return <h1>New Text</h1>;
     }
 }
 
--- project_setup/mern_setup/index.js
+++ project_setup/mern_setup/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const {resolve}= require('path');
 const PORT = process.env.PORT || 9000;
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+app.use(express.static(resolve(__dirname,'client','dist')));
 
 app.get('/api/test',(req,res) =>{
     res.send({
@@ -21,6 +25,20 @@ app.get ('/api/user', (req, res) => {
         email: 'richchigga@gmail.com',
         name: 'brian'
     });
+});
+
+app.post('/api/sign-in', (req,res) => {
+    console.log('Data from client:',req.body);
+
+    res.send({
+        success: true,
+        postData: req.body,
+        moreData:'Here is some more data'
+    });
+});
+
+app.get('*',(req,res) => {
+    res.sendFile(resolve(__dirname,'client','dist','index.html'))
 });
 
 app.listen(PORT, ()=>{
