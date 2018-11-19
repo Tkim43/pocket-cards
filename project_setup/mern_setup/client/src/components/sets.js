6ff099518aadacfa12f528f3cc95fecbac3dc4b4
Tiffany Kim <minikim9@hotmail.com>
2018-11-18 23:10:11 -0800

    editing routing setup

--- project_setup/mern_setup/client/src/components/sets.js
+++ project_setup/mern_setup/client/src/components/sets.js
@@ -1,38 +1,42 @@
 
 import React, {Component} from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Sets extends Component{
     constructor(props){
         super(props)
         this.state ={
+            data:[]
         }
     }
-    delete(){
-        this.setState({
-            delete:'delete'
-        })
+    componentDidMount(){
+        this.getUserData();
     }
+    // example async call
+    // async getUserData(){
+    //     try{
+    //         const resp = await axios.get(BASE_URL + API_KEY);
+    //         this.setState({
+    //             data: resp.data
+    //         });
+    //     }catch(err){
+    //         this.setState({
+    //             error: 'Error getting userData'
+    //         });
+    //     }
+    // }
     render(){
         return(
             <div>
                 <div className="border">
                     <h1>Sets</h1>
                 </div>
-                <Link>EDIT</Link>
+                <Link to ="/flashcards" className ="btn green darken-2">Chapter 1</Link>
+                <Link to ="/manageSets" className="btn green darken-2">Edit sets</Link>
             </div>
         )
     }
 }
 
 export default Sets;
-
-import React from 'react';
-
-
-export default props => (
-    <div>
-        <h1>My Sets</h1>
-        <p>lorem ipsum dummy datat</p>
-    </div>
-);
