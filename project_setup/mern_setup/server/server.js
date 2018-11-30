eba587251fc51cd45c027d5c60ce569965244129
Tiffany Kim <minikim9@hotmail.com>
2018-11-29 18:38:06 -0800

    changed the console.log

--- project_setup/mern_setup/server/server.js
+++ project_setup/mern_setup/server/server.js
@@ -10,8 +10,8 @@ const db = mysql.createConnection({
         'host': 'localhost',
         'user': 'root',
         'password': 'root',
-        'database': 'pocketcardstest',
-        'port': 8889,
+        'database': 'pocketcards',
+        'port': 3306,
         insecureAuth: true
     });
 
