2cb529aac4bd8e9a6b56860023557df9735caf17
Tiffany Kim <minikim9@hotmail.com>
2018-11-29 17:49:20 -0800

    changed branch to match port

--- project_setup/mern_setup/server/server.js
+++ project_setup/mern_setup/server/server.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
         'host': 'localhost',
         'user': 'root',
         'password': 'root',
-        'database': 'pocketcards',
+        'database': 'pocketcardstest',
         'port': 8889,
         insecureAuth: true
     });
