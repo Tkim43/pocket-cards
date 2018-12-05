8993e9db5e1fb0a02201320b0ef21543d03858e8
Tiffany Kim <minikim9@hotmail.com>
2018-12-04 16:35:03 -0800

    finished flashcard generation

--- project_setup/mern_setup/client/src/reducers/card_reducer.js
+++ project_setup/mern_setup/client/src/reducers/card_reducer.js
@@ -2,19 +2,20 @@ import types from '../actions/types';
 
 const DEFAULT_STATE = {
     back_description: '',
-    front_description: ''
+    front_description: '',
+    all_descriptions: []
 };
 
 export default (state = DEFAULT_STATE, action) => {
     switch (action.type){
+        case types.GET_ALL_CARD_DATA:
+            console.log("get all card data action:", action);
+            return {...state, all_descriptions: action.payload.data.data}
         case types.GET_CARD_DATA:
-            console.log("card data action", action.payload)
             return {...state, front_description: action.payload.data.data[0].frontText, back_description: action.payload.data.data[0].backText}
         case types.SEND_CARD_DATA:
-            console.log("Card sending action", action)
             return{...state, front_description: '', back_description: ''}
         default:
-            // console.log("card data action", action)
             return state;
     }
 }
