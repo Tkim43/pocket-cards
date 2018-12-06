9f3e2d9f45227120939f8eebe7d6d0c7058b8fcf
Tiffany Kim <minikim9@hotmail.com>
2018-12-05 17:48:39 -0800

    cleaned up changes

--- project_setup/mern_setup/client/src/components/sets.js
+++ project_setup/mern_setup/client/src/components/sets.js
@@ -1,7 +1,6 @@
 
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import '../assets/css/sets.css'
 import { connect } from 'react-redux';
 import {getSetsData} from '../actions';
