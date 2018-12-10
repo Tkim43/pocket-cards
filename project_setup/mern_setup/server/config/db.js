ed67317c6e24f88f640b5c71e494ea71cf42c2e4
Tiffany Kim <minikim9@hotmail.com>
2018-12-10 12:45:05 -0800

    git ignored the db file

--- project_setup/mern_setup/server/config/db.js
+++ project_setup/mern_setup/server/config/db.js
@@ -1,13 +1,6 @@
 module.exports = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'root',
+    'host': 'tiffanyykim.com',
+    'user': 'pocket_cards',
+    'password': 'mRN7Us9^7p5YdCwu',
     'database': 'pocketcards'
 }
-
-// module.exports = {
-//     'host': 'tiffanyykim.com',
-//     'user': 'pocket_cards',
-//     'password': 'mRN7Us9^7p5YdCwu',
-//     'database': 'pocketcards'
-// }
