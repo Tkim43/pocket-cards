a47f0ec8feecd100db36467241f3949b4c898fcf
Brandon Park <brandonpark206@gmail.com>
2018-12-10 21:32:07 -0800

    added redux form

--- project_setup/mern_setup/client/src/actions/index.js
+++ project_setup/mern_setup/client/src/actions/index.js
@@ -205,3 +205,4 @@ export function sendCreateCardData(createCard){
         payload: {frontText, backText}
     }
 }
+
--- project_setup/mern_setup/client/src/components/input.js
+++ project_setup/mern_setup/client/src/components/input.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-export default ({input, label, meta: {error, touched}}) => (
-    <div>
-        <input {...input} type = "text"/>
-        <label htmlFor="">{label}</label>
+export default ({input, label, size = 's12'}) => (
+    <div className = {`input-field col ${size}`}>
+        <input {...input} id = {input.name} autoComplete = "off"/>
+        <label htmlFor={input.name}>{label}</label>
     </div>
-    
 );
