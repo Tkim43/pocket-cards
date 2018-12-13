29914b790f02c0b40be74592165ccea142caa0c1
Brandon Park <brandonpark206@gmail.com>
2018-12-12 16:14:57 -0800

    deletion of console logs and cleaning up the code

--- client/src/components/findTimePassed.js
+++ client/src/components/findTimePassed.js
@@ -6,69 +6,29 @@ class FindTimePassed extends Component {
         super(props);
 
         this.timezoneOffset = new Date().getTimezoneOffset();
-        debugger;
-        // const created = new Date(props.created.slice(0, -1) + Math.floor(this.timezoneOffset/60));
-        // console.log('props.created: ', props.created);
-        // console.log('created: ', created);
         this.state = { timePassed: props.created};
-
-        // console.log('Initial State:', this.state);
-    }
-
-    componentDidMount () {
-        // this.setTimePassed();
-        // this.findTimePassedID = setInterval (
-        //     () => this.setTimePassed(), 10000
-        // );
-    }
-
-    componentWillUnmount () {
-        clearInterval (this.findTimePassedID);
-    }
-
-    setTimePassed = () => {
-        var timeCreated = new Date(this.props.created.slice(0, -1) + Math.floor(this.timezoneOffset/60));
-        // var date = new Date ();
-        // console.log("date:         ", date, "\ntime created: ", timeCreated);
-        // var now = date.getTime();
-        var result = Date.now() - timeCreated;
-        this.setState({ timePassed: result });
     }
 
     findTimePassed = () => {
         const { timePassed } = this.state;
-        // console.log('TIME PASSED:', timePassed);
         if(timePassed >= 86400000){
             var numOfDays = timePassed / 86400000;
-            // this.setState({timePassed: numOfDays});
-            // console.log("num of days: ", numOfDays);
             return `Updated : ${Math.floor(numOfDays)} days ago`;
         }
         else if(timePassed >= 3600000){
             var numOfHours = timePassed / 3600000;
-            // this.setState({timePassed: numOfHours});
-            // console.log("num of hrs: ", numOfHours);
             return `Updated : ${Math.floor(numOfHours)} hours ago`;
         }
         else if(timePassed >= 60000){
             var numOfMinutes = timePassed / 60000;
-            // this.setState({timePassed: numOfMinutes});
-            // console.log("num of mins: ", numOfMinutes);
             return `Updated : ${Math.floor(numOfMinutes)} minutes ago`;
         }
-        // else if (result >= 1000){
-        //     var numOfSeconds = result / 1000;
-        //     this.setState({timePassed: numOfSeconds});
-        //     console.log("num of seconds: ", numOfSeconds);
-        //     return `Updated : ${Math.floor(this.state.timePassed)} seconds ago`;
-        // }
         else {
             return "Updated : Now";
         }
     }
 
     render () {
-        // console.log("THIS IS STATE: ", this.state);
 
         return (
             <div className = "col s12 white-text">{this.findTimePassed()}</div>
--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -31,20 +31,11 @@ class Profile extends Component {
         const profileCategories =  this.props.sets.map ( (item, ID) => {
 
             const created = new Date(item.created).getTime();
-            
             const ms = created - timeZoneOffset;
-
             const diff = new Date().getTime() - ms;
-
-            console.log('TZ OFFSET:', timeZoneOffset);
-            console.log('CREATED:', created);
-            console.log('Diff:', diff);
-
-            console.log('ID:', item.ID,  'Created:', new Date(ms));
 
             return (
                 <div className="row category-info" key = {item.ID}>
-                    {/* <div className = "col s12 white-text">{this.findTimePassed(item)}</div> */}
                     <FindTimePassed created={diff}/>
                     <div className="col s12 card-container">
                         <Link to = {`/sets/${item.ID}`} className = "card-panel green lighten-2 white-text center sets-bold-text">{item.category}</Link>
