50019e78506e9d268ca952ce4eeaf173488b0c9b
Tiffany Kim <minikim9@hotmail.com>
2018-12-18 12:45:41 -0800

    completed delete modal

--- client/src/components/sets.js
+++ client/src/components/sets.js
@@ -6,17 +6,26 @@ import { connect } from 'react-redux';
 import { getSetsData , deleteSubcategory, createSubcategory} from '../actions';
 
 class Sets extends Component{
-    state = {show: false};
+    state = {
+        show: false,
+        delete: false
+    };
+    
     componentDidMount(){
         this.props.getSetsData(this.props.match.params.set_id);
     }
+
     delete = async (topicID, setID)=>{
         const {deleteSubcategory} = this.props
         await deleteSubcategory(topicID, setID);
         
         this.props.getSetsData(this.props.match.params.set_id);
+        this.setState({
+            delete: true
+        })
         
     }
+
     async updateSubcategoryList(){
         try{
             const {getSetsData, match: {params}} = this.props
@@ -25,16 +34,13 @@ class Sets extends Component{
             console.log("error subcategory list")
         }
     }
-    showModal = () =>{
-        this.setState({
-            show: true
-        })
-    }
+
     updateSubCategory = event =>{
         this.setState({
             subCategory: event.currentTarget.value
         });
     }
+
     createSubcategory  = (e) => {
         e.preventDefault();
         const {createSubcategory, match:{params}} = this.props
@@ -42,13 +48,58 @@ class Sets extends Component{
         this.hideModal();
         this.updateSubcategoryList();
     }
+
+    showModal = (event) =>{
+        const buttonType = event.target.classList[0];
+        let show = null;
+        let remove = null;
+
+        if (buttonType === 'green') {
+            show = true;
+            remove = false;
+        } else {
+            show = false;
+            remove = true;
+        }
+        this.setState({
+            show: show,
+            delete: remove
+        });
+    }
+
     hideModal = () =>{
         this.setState({
-            show: false
+            show: false,
+            delete: false
         })
     }
+
     render(){
         if(this.state.show){
+            return (
+                <div className="basic-modal" onClick={this.hideModal}>
+                    <div onClick={e => e.stopPropagation()} className="basic-modal-content">
+                        <div onClick={this.hideModal} className="basic-modal-close center">X</div>
+                        <div>
+                            <form className="col s12">
+                                <div className="row"> 
+                                    <div className="input-field col s12">
+                                        <textarea onChange ={this.updateSubCategory} className="materialize-textarea" id="textarea2"></textarea>
+                                        <label htmlFor="textarea2">Create Title</label>
+                                    </div>  
+                                </div>
+                                <div className = "row">
+                                    <button onClick = {this.createSubcategory}className="green lighten-2 btn waves-effect waves-light btn-large" type="done" name="action">
+                                        Done
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        if(this.state.delete){
             return (
                 <div className="basic-modal" onClick={this.hideModal}>
                     <div onClick={e => e.stopPropagation()} className="basic-modal-content">
@@ -57,14 +108,10 @@ class Sets extends Component{
                                 <form className="col s12">
                                         <div className="row"> 
                                             <div className="input-field col s12">
-                                                <textarea onChange ={this.updateSubCategory} className="materialize-textarea" id="textarea2"></textarea>
-                                                <label htmlFor="textarea2">Create Title</label>
+                                                <p>Are you sure?</p>
+                                                <button onClick={() => this.delete(item.topicID, item.setID)}>Yes</button>
+                                                <button onClick={this.hideModal}>No</button>
                                             </div>  
-                                        </div>
-                                        <div className = "row">
-                                            <button onClick = {this.createSubcategory}className="green lighten-2 btn waves-effect waves-light btn-large" type="done" name="action">
-                                                Done
-                                            </button>
                                         </div>
                                 </form>
                             </div>
@@ -78,9 +125,11 @@ class Sets extends Component{
             return(
                 <div key= {item.setID} className="row set">
                     <Link to={`/displayCard/${item.setID}/topic/${item.topicID}/card/0`} className ="btn blue darken-3 ">{item.subCategory}</Link>
-                    <button className="red lighten-2 btn-large" onClick={() => this.delete(item.topicID, item.setID)}>
+                    <div className = "row">
+                    <button className="red lighten-2 btn-large" onClick ={this.showModal}>
                         <i className = "large material-icons">delete</i>
                     </button>
+                    </div>
                 </div>
             );  
         });
@@ -93,7 +142,7 @@ class Sets extends Component{
                 </div>
                 <div className="row">
                     <div className="col s12 center">
-                    <button onClick = {this.showModal} className="btn green lighten-2 wide-btn" >Add Title</button>
+                    <button onClick = {this.showModal} className="green btn lighten-2 wide-btn" >Add Title</button>
                         <Link to="/profile" className="btn yellow darken-2 wide-btn">Home</Link>
                     </div>
                 </div>
