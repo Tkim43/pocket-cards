3748fc4cac86603c76c3cf13f99a1a21979fa84b
viennaltran <lanvienhtran@gmail.com>
2018-12-18 14:53:17 -0800

    skeleton for numeric denote done

--- client/src/components/displayCard.js
+++ client/src/components/displayCard.js
@@ -49,8 +49,8 @@ class displayCard extends Component{
                     <h3 className = "white-text">{flipped ? "Front of Card" : "Back of Card"}</h3>
                 </div> */}
                 <div className={"cardBox flow-text center cardflip " + (flipped ? "" : "flipped")}>
-                    <p id ="cardFont" className="front flow-text">{card.frontText}</p>
-                    <p id ="cardFont" className="back flow-text">{isLoading ? '' : card.backText}</p>
+                    <p id ="cardFont" className="front flow-text"><span className="numeric">1a</span>{card.frontText}</p>
+                    <p id ="cardFont" className="back flow-text"><span className="numeric">1b</span>{isLoading ? '' : card.backText}</p>
                 </div>
                 <div className="row col s12">
                     <i className="nav-btn large material-icons white-text" onClick={() => this.nextPrevCard('previous')}>arrow_back</i>
